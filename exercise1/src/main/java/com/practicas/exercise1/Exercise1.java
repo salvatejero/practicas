3545f800577e48fefeaf701669214b0c828d59f4
Salva Tejero <salva.tejero@gmail.com>
2020-03-16 13:16:35 +0100

    Ejercicio 1, apartado 2

--- exercise1/src/main/java/com/practicas/exercise1/Exercise1.java
+++ exercise1/src/main/java/com/practicas/exercise1/Exercise1.java
@@ -8,8 +8,21 @@ public class Exercise1 {
 
         System.out.println("Exercise 1");
         
-        System.out.println("RESULT  " +  CarService.getMarcaModelo(25, 33));
+        System.out.println("Exercise 1.1");
+        
+        System.out.println("RESULT 1.1 " +  CarService.getMarcaModelo(25, 33));
 
+        System.out.println("Exercise 1.2");
+        
+        
+        System.out.println("RESULT  1.2 - 1" +  CarService.getMarcaModeloGTHorsePower(10, 150));
+        System.out.println("RESULT  1.2 - 2" +  CarService.getMarcaModeloGTHorsePower2(10, 150));
+        System.out.println("RESULT  1.2 - 3" +  CarService.getMarcaModeloGTHorsePower2(10, 150));
+        
         System.out.println("Exercise 1 END");
+        
+        
+        
+        
     }
 }
--- exercise1/src/main/java/com/practicas/exercise1/service/CarService.java
+++ exercise1/src/main/java/com/practicas/exercise1/service/CarService.java
@@ -1,5 +1,7 @@
 package com.practicas.exercise1.service;
 
+import java.util.Iterator;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -33,8 +35,93 @@ public class CarService {
 		}
 		
 		return arrayReturn;
-		
-		
 	}
+	
+	/**
+	 * Devuelve un listado de Marca y Modelo con más caballos {horsepower}
+	 * Usando Iterator
+	 * @param count Número de elementos
+	 * @param horsepower Control
+	 * @return
+	 */
+	public static JSONArray getMarcaModeloGTHorsePower(int count, int horsepower) {
+		
+		if (horsepower < 0) {
+			return null;
+		}
+		JSONArray array = DatabaseJson.loadDatabase().getData();
+		JSONArray arrayReturn = new JSONArray();
+		Iterator<Object> iterator = array.iterator();
+		while(iterator.hasNext() && arrayReturn.length() < count) {
+			JSONObject jObj = (JSONObject) iterator.next();
+			if(jObj.getJSONObject("Engine Information").getJSONObject("Engine Statistics").getInt("Horsepower") > 150) {
+				JSONObject objReturn = new JSONObject();
+				objReturn.put("make", jObj.getJSONObject("Identification").getString("Make"));
+				objReturn.put("model", jObj.getJSONObject("Identification").getString("ID"));
+		    	arrayReturn.put(objReturn);
+		    }
+		}
+		return arrayReturn;
+	}
+	
+	/**
+	 * Devuelve un listado de Marca y Modelo con más caballos {horsepower}
+	 * Usando FOR
+	 * @param count Número de elementos
+	 * @param horsepower Control
+	 * @return
+	 */
+	public static JSONArray getMarcaModeloGTHorsePower2(int count, int horsepower) {
+		
+		if (horsepower < 0) {
+			return null;
+		}
+		JSONArray array = DatabaseJson.loadDatabase().getData();
+		JSONArray arrayReturn = new JSONArray();
+		for(int i=0; i<array.length() || i < count; i++) {
+			JSONObject jObj = (JSONObject) array.get(i);
+			if(jObj.getJSONObject("Engine Information").getJSONObject("Engine Statistics").getInt("Horsepower") > 150) {
+		    	JSONObject objReturn = new JSONObject();
+				objReturn.put("make", jObj.getJSONObject("Identification").getString("Make"));
+				objReturn.put("model", jObj.getJSONObject("Identification").getString("ID"));
+		    	arrayReturn.put(objReturn);
+		    }
+		}
+			
+		return arrayReturn;
+	}
+	
+	
+	/**
+	 * Devuelve un listado de Marca y Modelo con más caballos {horsepower}
+	 * Usando forEach
+	 * @param count Número de elementos
+	 * @param horsepower Control
+	 * @return
+	 */
+	public static JSONArray getMarcaModeloGTHorsePower3(int count, int horsepower) {
+	
+		if (horsepower < 0) {
+			return null;
+		}
+		JSONArray array = DatabaseJson.loadDatabase().getData();
+		JSONArray arrayReturn = new JSONArray();
+		array.forEach(item -> {
+			JSONObject jObj = (JSONObject) item;
+		    if(jObj.getJSONObject("Engine Information").getJSONObject("Engine Statistics").getInt("Horsepower") > horsepower) {
+		    	JSONObject objReturn = new JSONObject();
+				objReturn.put("make", jObj.getJSONObject("Identification").getString("Make"));
+				objReturn.put("model", jObj.getJSONObject("Identification").getString("ID"));
+		    	arrayReturn.put(objReturn);
+		    }
+		    if(arrayReturn.length() >= count) {
+		    	return;
+		    }
+		});
+		
+		return arrayReturn;
+	}
+	
+	
 
 }
